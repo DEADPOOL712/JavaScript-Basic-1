9db470832f5096602d4c48cfc6a775a96fcc4566
vaibhav <98164944+DEADPOOL712@users.noreply.github.com>
2022-05-30 08:49:58 +0530

    Add files via upload

---
+++ 1)values-and-variable/assignment.js
@@ -0,0 +1,7 @@
+let country = "india";
+let continet = "Asia";
+let population = "1,380,004,385";
+
+console.log(country);
+console.log(continet);
+console.log(population);
---
+++ 1)values-and-variable/script.js
@@ -0,0 +1,22 @@
+let js = "amazing";
+// if (js == 'amazing') alert("javaScript is fun");
+40 + 8 + 5 // this is cannot give answer because this is no script file not on consloe  
+console.log(40 + 8 - 10); // for get output we use this formate 
+
+// value : The smallest unit of information 
+console.log("john");
+console.log(45);
+// variable : store the value 
+let firstname = "vaibhav";
+let firstName = "VAIBHAV";
+// let 3year = 3;  --> cannot start with number 
+// don't start variable name with uppercase 
+// variable in all uppercase are use for constant 
+let PI = 3.1415;
+// name the meaningfull variable  
+let myFirstJob = "i dont have job i am student ";
+console.log(myFirstJob);
+console.log(firstname);
+console.log(firstName);
+
+let thisisname = "those who dare"
